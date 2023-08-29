816cb425cf0948b6844e870c1555e7f14eb7d4a2
PQMIT <PQM2112000@gmail.com>
2023-08-29 16:14:53 +0700

    init page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,135 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="container">
+      <strong>Lưu ý:</strong> Mỗi thiết bị sẽ chỉ vượt được 1 link/1 nhiệm vụ/1
+      ngày
+      <p />
+      <hr />
+      <div className="row nhiemvu text-center ">
+        <div className="col-md-3 col-sm-6 col-6 py-1">
+          <a
+            href="https://dilink.net/DoCxV"
+            target="_blank"
+            rel="noreferrer"
+            className="btn btn-mod btn-border btn-medium animation-one"
+          >
+            <span>Dilink</span>
+          </a>
+        </div>
+        <div className="col-md-3 col-sm-6 col-6 py-1">
+          <a
+            href="https://traffic123.net/thBsx"
+            target="_blank"
+            rel="noreferrer"
+            className="btn btn-mod btn-border btn-medium animation-one"
+          >
+            <span>Traffic123</span>
+          </a>
+        </div>
+        <div className="col-md-3 col-sm-6 col-6 py-1">
+          <a
+            href="https://beelink.life/a1t1rw"
+            target="_blank"
+            rel="noreferrer"
+            className="btn btn-mod btn-border btn-medium animation-one"
+          >
+            <span>Beelink 1</span>
+          </a>
+        </div>
+        <div className="col-md-3 col-sm-6 col-6 py-1">
+          <a
+            href="https://beelink.life/qejrwo"
+            target="_blank"
+            rel="noreferrer"
+            className="btn btn-mod btn-border btn-medium animation-one"
+          >
+            <span>Beelink 2</span>
+          </a>
+        </div>
+        <hr />
+        <div className="col-md-3 col-sm-4 col-4 py-1">
+          <a
+            href="https://taixiugames.com/agency?id=41218&platform=sport"
+            target="_blank"
+            rel="noreferrer"
+            className="btn btn-mod btn-border btn-medium animation-one"
+          >
+            <span>Sport</span>
+          </a>
+        </div>
+        <div className="col-md-3 col-sm-4 col-4 py-1">
+          <a
+            href="https://taixiugames.com/agency?id=95396&platform=casino"
+            target="_blank"
+            rel="noreferrer"
+            className="btn btn-mod btn-border btn-medium animation-one"
+          >
+            <span>Casino</span>
+          </a>
+        </div>
+        <div className="col-md-3 col-sm-4 col-4 py-1">
+          <a
+            href="https://taixiugames.com/agency?id=60272&platform=Game3D"
+            target="_blank"
+            rel="noreferrer"
+            className="btn btn-mod btn-border btn-medium animation-one"
+          >
+            <span>Game3d</span>
+          </a>
+        </div>
+        <div className="col-md-3 col-sm-4 col-4 py-1">
+          <a
+            href="https://taixiugames.com/agency?id=18225&platform=banca"
+            target="_blank"
+            rel="noreferrer"
+            className="btn btn-mod btn-border btn-medium animation-one"
+          >
+            <span>Bắn cá</span>
+          </a>
+        </div>
+        <div className="col-md-3 col-sm-4 col-4 py-1">
+          <a
+            href="https://taixiugames.com/agency?id=39563&platform=nohu"
+            target="_blank"
+            rel="noreferrer"
+            className="btn btn-mod btn-border btn-medium animation-one"
+          >
+            <span>Nổ hũ</span>
+          </a>
+        </div>
+        <div className="col-md-3 col-sm-4 col-4 py-1">
+          <a
+            href="https://taixiugames.com/agency?id=94015&platform=xoso"
+            target="_blank"
+            rel="noreferrer"
+            className="btn btn-mod btn-border btn-medium animation-one"
+          >
+            <span>Xổ số</span>
+          </a>
+        </div>
+      </div>
+      <div className="container">
+        <i className="fa-brands fa-facebook-messenger" />
+        <i className="fa-brands fa-telegram" />
+        <i className="fas fa-camera" />
+        <span className="fas fa-camera" />
+      </div>
+      <hr />
+      <div className="thongbao">
+        <div className="container">
+          <h3>Thông báo :</h3>
+          <p>
+            Sau khi làm xong 1 nhiệm vụ hãy chụp màn hình gửi Admin để nhận
+            tiền. 300đ/1nvu sẽ thanh toán ngay khi đang admin online. Khi Nhận
+            Nhiệm Vụ mà chuyển hướng ngay tức là hết Nhiệm Vụ. Nếu bạn đã làm
+            nhiệm vụ của 1 web (ví dụ Dilink) thì không nên làm nhiệm vụ đó nữa
+            mà hãy làm nhiệm vụ khác, vì làm sẽ không tính view &amp; tính tiền.
+          </p>
+        </div>
+      </div>
+    </main>
   );
 }
 
